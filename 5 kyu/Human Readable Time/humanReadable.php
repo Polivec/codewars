a345386813b25bcaea55a31460a0bad262cd3e4e
Tomáš Polívka <tomas.polivka@sportisimo.cz>
2024-05-02 14:35:59 +0200

    human_readable

---
+++ 5 kyu/Human Readable Time/humanReadable.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types = 1);
+/**
+ * @copyright (c) 2024 Sportisimo s.r.o.
+ * @author        tomas.polivka
+ * @since         2024-05-02
+ */
+
+function human_readable(int $seconds) : string {
+  $hours = floor($seconds / 3600);
+  $minutes = floor(($seconds % 3600) / 60);
+  $seconds = $seconds % 60;
+  return sprintf('%02d:%02d:%02d', $hours, $minutes, $seconds);
+}
